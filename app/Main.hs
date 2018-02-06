10a7a05440ea767a3d0aa9204baca35c7aad6a7e
Nyiro Gergo <gergo.nyiro@balabit.com>
2018-02-06 17:10:35 +0100

    multiple user

--- app/Main.hs
+++ app/Main.hs
@@ -2,43 +2,53 @@ module Main where
 
 import Lib
 import Control.Concurrent (forkIO, threadDelay)
-import Control.Concurrent.STM (TChan, atomically, writeTChan, readTChan, newTChanIO)
+import Control.Concurrent.STM (TChan, atomically, writeTChan, readTChan, newTChanIO, newBroadcastTChan, dupTChan)
 import Network.Socket
 
 type CicaChannel = TChan String
 
 main :: IO ()
 main = do
-    chan <- newTChanIO :: IO CicaChannel
-    _ <- forkIO $ generate chan
-    _ <- forkIO $ consume chan
+    readerChannel <- newTChanIO :: IO CicaChannel
+    broadcastChannel <- atomically newBroadcastTChan :: IO CicaChannel
+    _ <- forkIO $ forwardMessage readerChannel broadcastChannel
     sock <- createSocket
-    forever $ forwardMessage sock chan
+    acceptSocket sock readerChannel broadcastChannel
 
-generate :: CicaChannel -> IO ()
-generate channel = forever $ do
-    atomically $ writeTChan channel "cica"
-    threadDelay 1000000
+consume :: Socket -> CicaChannel -> IO ()
+consume sock channel = forever $ do
+    msg <- recv sock 122
+    atomically $ writeTChan channel msg
 
-consume :: CicaChannel -> IO ()
-consume channel = forever $ do
-    s <- atomically $ readTChan channel
-    putStrLn s
+produce :: Socket -> CicaChannel -> IO ()
+produce sock channel = forever $ do
+    msg <- atomically $ readTChan channel
+
+    send sock msg
+    pure ()
 
 forever :: Monad a => a () -> a ()
 forever u = do
     u
     forever u
 
-forwardMessage :: Socket -> CicaChannel -> IO ()
-forwardMessage sock channel = do
-    msg <- recv sock 100
-    atomically $ writeTChan channel msg
+forwardMessage :: CicaChannel -> CicaChannel -> IO ()
+forwardMessage readerChannel broadcastChannel = forever $ do
+    msg <- atomically $ readTChan readerChannel
+    atomically $ writeTChan broadcastChannel msg
 
 createSocket :: IO Socket
 createSocket = do
   sock <- socket AF_INET Stream 0
   setSocketOption sock ReuseAddr 1
   bind sock (SockAddrInet 4242 iNADDR_ANY)
-  listen sock 2
-  fst <$> accept sock
+  listen sock 5
+  pure sock
+
+acceptSocket :: Socket -> CicaChannel -> CicaChannel -> IO ()
+acceptSocket listeningSocket readerChannel broadcastChannel = forever $ do
+    sock <- fst <$> accept listeningSocket
+    _ <- forkIO $ consume sock readerChannel
+    bb <- atomically $ dupTChan broadcastChannel
+    _ <- forkIO $ produce sock bb
+    pure ()
